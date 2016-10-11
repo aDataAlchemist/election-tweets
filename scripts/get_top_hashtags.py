0472c1cabdfdf0f8a193552dac3370ae93bbdaed
Conal Sathi <conals@slice.com>
2016-10-10 22:17:39 -0700

    Use a more compact functional style for instantiating hashtagCounter

--- scripts/get_top_hashtags.py
+++ scripts/get_top_hashtags.py
@@ -4,10 +4,7 @@ from collections import Counter
 
 f = open(sys.argv[1], 'r')
 topk = int(sys.argv[2]) if len(sys.argv) > 2 and sys.argv[2].isdigit() else 10
-hashtags = []
-for line in f:
-    if line.startswith('{'):
-        hashtags.extend(json.loads(line)['hashtags'])
-hashtagCounter = Counter([hashtag.lower() for hashtag in hashtags])
+
+hashtagCounter = Counter([hashtag.lower() for line in f if line.startswith('{') for hashtag in json.loads(line)['hashtags'] ])
 for (hashtag, count) in hashtagCounter.most_common(topk):
     print hashtag, count
